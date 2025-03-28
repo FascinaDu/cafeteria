962885eb59b290d4ecd53e204709dc1845a6fafe
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-28 19:19:52 -0300

    Summary

--- scripts.js
+++ scripts.js
@@ -5,3 +5,16 @@
         []
 
 */
+
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector('.mascara-modal')
+
+function mostrarModal() {
+        modal.style.top = '50%'
+        mascara.style.visibility = 'visible'
+}
+
+function esconderModal() {
+        modal.style.top = '-40%'
+        mascara.style.visibility = 'hidden'
+}
