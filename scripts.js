540a1fd8600e3e9f191a2643454e51c3069b96aa
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-04-02 00:01:37 -0300

    summary

--- scripts.js
+++ scripts.js
@@ -2,11 +2,20 @@ const modal = document.querySelector(".modal")
 const mascara = document.querySelector('.mascara-modal')
 
 function mostrarModal() {
-        modal.style.top = '50%'
-        mascara.style.visibility = 'visible'
+    modal.style.top = '50%';
+    modal.style.opacity = '1';
+    modal.style.visibility = 'visible'; 
+
+    mascara.style.opacity = '1';
+    mascara.style.visibility = 'visible';
 }
 
 function esconderModal() {
-        modal.style.top = '-40%'
-        mascara.style.visibility = 'hidden'
+    modal.style.top = '-40%';
+    modal.style.opacity = '0';
+    modal.style.visibility = 'hidden'; 
+
+    mascara.style.opacity = '0';
+    mascara.style.visibility = 'hidden';
 }
+
